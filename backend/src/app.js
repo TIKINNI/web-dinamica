8304083395d4e423c172646bed67e62c770483d9
nicojacz <jacznik77@gmail.com>
2020-11-28 16:14:54 -0300

    Añadido inicio de web usando ejs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const noticiasRoute = require('./routes/noticias_route');
+const path = require('path');
+
+
 
 const app = express();
 const port = 3000;
 app.use(express.json());
+
+app.use(express.static('public'));
+
+
+app.get('/home', (req, res) => {
+    res.render(path.join(__dirname, 'views/home.ejs'));
+});
 
 //routes
 app.use('/noticias', noticiasRoute);
@@ -13,3 +23,5 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
+
