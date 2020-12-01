843a23cc08239722866dcdd12c161cda4f1aa9cf
nicojacz <jacznik77@gmail.com>
2020-12-01 17:49:02 -0300

    probando ejs pero no anduvo :(

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,11 +8,23 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.use(express.static('public'));
+    // Require static assets from public folder
+    app.use(express.static(path.join(__dirname, 'public')));
+    // Set view engine as EJS
+    app.engine('ejs', require('ejs').renderFile);
+    app.set('view engine', 'ejs');
+    // Set 'views' directory for any views 
+    // being rendered res.render()
+    app.set('views', path.join(__dirname, ''));
+    app.use('/form', express.static(__dirname + '/index.html'));
 
 
 app.get('/home', (req, res) => {
     res.render(path.join(__dirname, 'views/home.ejs'));
+});
+
+app.get('/:anio/:mes/:dia', (req, res) => {
+    res.render(path.join(__dirname, 'views', req.params.anio, req.params.mes, req.params.dia));
 });
 
 //routes
